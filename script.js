05c5df779575ffca097a72a4c08d6e71f272c5ba
lloydue <johnlloydalimboyao08@gmail.com>
2025-06-15 16:39:08 +0800

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 80
+      },
+      size: {
+        value: 3
+      },
+      move: {
+        speed: 2
+      },
+      line_linked: {
+        enable: true
+      }
+    }
+  });
+  
